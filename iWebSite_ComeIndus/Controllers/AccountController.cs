93eba2682ec94d6bf74f6bc250915e5d1cb3b375
csi1212csi1212@gmail.com <csi1212csi1212@gmail.com>
2020-12-06 23:09:06 +0800

    註冊功能、BaseController核心增加。

--- iWebSite_ComeIndus/Controllers/AccountController.cs
+++ iWebSite_ComeIndus/Controllers/AccountController.cs
@@ -42,5 +42,79 @@ namespace iWebSite_ComeIndus.Controllers
 
             //return Redirect("/Account/Login");
         }
+
+        /// <summary>
+        /// 登入頁面View
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登入驗證
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Register(Member Model)
+        {
+            Model.MailCheck = "0";
+            Model.Authority = "0";
+            Model.PwdChangeCheck = "0";
+
+            //SQL Insert Member
+            var sqlStr = string.Format(
+                @"INSERT INTO [dbo].[Member] (" +
+                    "[Account]," +
+                    "[Password]," +
+                    "[Username]," +
+                    "[Gender]," +
+                    "[Birthday]," +
+                    "[PhotoURL]," +
+                    "[MailCheck]," +
+                    "[PwdChangeCheck]," +
+                    "[CreateTime]," +
+                    "[AccountStart]," +
+                    "[Authority]" +
+                ")VALUES(" +
+                    "{0}," +
+                    "{1}," +
+                    "{2}," +
+                    "{3}," +
+                    "{4}," +
+                    "{5}," +
+                    "{6}," +
+                    "{7}," +
+                    "{8}," +
+                    "{9}," +
+                    "{10}",
+                    SqlVal2(Model.Account),
+                    SqlVal2(Model.Password),
+                    SqlVal2(Model.Username),
+                    SqlVal2(Model.Gender),
+                    SqlVal2(Model.Birthday),
+                    SqlVal2(Model.PhotoURL),
+                    SqlVal2(Model.MailCheck),
+                    SqlVal2(Model.PwdChangeCheck),
+                    "getDate()",
+                    "getDate()",
+                    SqlVal2(Model.Authority)+")"
+                );
+
+            //SQL Check
+            var check = _DB_Execute(sqlStr);
+
+            //新增是否成功
+            if(check == 1)
+            {
+                return Redirect("/Home/index");
+            }
+            else
+            {
+                return View(Model);
+            }
+        }
     }
 }
